2dae17a106c160c7502b30bbcf5d91e1d85bf4b1
Patrick Kim <pkim050@gmail.com>
2019-06-08 01:16:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const ranks = document.querySelectorAll('.ranked-list li');
   for (let i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt((n + ranks[i].innerHTML).toString(),10);
+    ranks[i].innerHTML = n + parseInt(rank[i].innerHTML);
   }
   return ranks;
 }
